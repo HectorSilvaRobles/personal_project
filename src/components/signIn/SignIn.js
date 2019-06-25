9f53ef295f7d98206ccb76e0d86b2e08fba84a44
HSR <hectorsilvarobles@gmail.com>
2019-06-25 09:18:18 -0700

    added alt text to sign in image

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -7,7 +7,7 @@ class SignIn extends Component {
     render() {
         return (
             <div className='page'>
-            <img src='https://i.pinimg.com/originals/5d/10/d9/5d10d98db2f88b601b55c0809e598f26.png' />
+            <img src='https://i.pinimg.com/originals/5d/10/d9/5d10d98db2f88b601b55c0809e598f26.png' alt='logo' />
                 <div id='sign-in'>
                     <div id='login'><Login /></div>
                     <div id='register'><Register /></div>
